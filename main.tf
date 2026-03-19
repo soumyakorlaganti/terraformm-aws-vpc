7a1efe2d39fb6a387a79a744b3110301acb76656
soumya <korlagani.soumya@gmail.com>
2026-03-19 13:48:09 +0530

    terraform

--- main.tf
+++ main.tf
@@ -9,7 +9,5 @@ resource "aws_vpc" "main" {
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id # VPC association
 
-  tags = {
-    Name = "main"
-  }
+  tags = local.igw_final_tags
 }
