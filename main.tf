4039de3ef7cef63606b334c06808aada16f3bfd9
soumya <korlagani.soumya@gmail.com>
2026-03-20 16:18:58 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "main" {
-  cidr_block       = var.vpc_cidr 
+  cidr_block       = var.vpc_cidr
   instance_tenancy = "default"
   enable_dns_hostnames = true
 
@@ -15,65 +15,65 @@ resource "aws_internet_gateway" "main" {
 # Public Subnets
 resource "aws_subnet" "public" {
   count = length(var.public_subnet_cidrs)
-  vpc_id = aws_vpc.main.id
+  vpc_id     = aws_vpc.main.id
   cidr_block = var.public_subnet_cidrs[count.index]
   availability_zone = local.az_names[count.index]
   map_public_ip_on_launch = true
 
   tags = merge(
-    local.common_tags,
-    # roboshop-dev-public-us-east-1a
-    {
-      Name = "${var.project}-${var.environment}-public-${local.az_names[count.index]}"
-    },
-    var.public_subnet_tags
-  )
+        local.common_tags,
+        # roboshop-dev-public-us-east-1a
+        {
+            Name = "${var.project}-${var.environment}-public-${local.az_names[count.index]}"
+        },
+        var.public_subnet_tags
+    )
 }
 
-# Private Subnets
+# private Subnets
 resource "aws_subnet" "private" {
   count = length(var.private_subnet_cidrs)
-  vpc_id = aws_vpc.main.id
+  vpc_id     = aws_vpc.main.id
   cidr_block = var.private_subnet_cidrs[count.index]
   availability_zone = local.az_names[count.index]
 
   tags = merge(
-    local.common_tags,
-    # roboshop-dev-private-us-east-1a
-    {
-      Name = "${var.project}-${var.environment}-private-${local.az_names[count.index]}"
-    },
-    var.private_subnet_tags
-  )
+        local.common_tags,
+        # roboshop-dev-private-us-east-1a
+        {
+            Name = "${var.project}-${var.environment}-private-${local.az_names[count.index]}"
+        },
+        var.private_subnet_tags
+    )
 }
 
 # database Subnets
 resource "aws_subnet" "database" {
   count = length(var.database_subnet_cidrs)
-  vpc_id = aws_vpc.main.id
+  vpc_id     = aws_vpc.main.id
   cidr_block = var.database_subnet_cidrs[count.index]
   availability_zone = local.az_names[count.index]
 
   tags = merge(
-    local.common_tags,
-    # roboshop-dev-database-us-east-1a
-    {
-      Name = "${var.project}-${var.environment}-database-${local.az_names[count.index]}"
-    },
-    var.database_subnet_tags
-  )
+        local.common_tags,
+        # roboshop-dev-database-us-east-1a
+        {
+            Name = "${var.project}-${var.environment}-database-${local.az_names[count.index]}"
+        },
+        var.database_subnet_tags
+    )
 }
 
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
   tags = merge(
-    local.common_tags,
-    # roboshop-dev-public
-    {
-      Name = "${var.project}-${var.environment}-public"
-    },
-    var.public_route_table_tags
+        local.common_tags,
+        # roboshop-dev-public
+        {
+            Name = "${var.project}-${var.environment}-public"
+        },
+        var.public_route_table_tags
   )
 }
 
@@ -81,12 +81,12 @@ resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
   tags = merge(
-    local.common_tags,
-    # roboshop-dev-private
-    {
-      Name = "${var.project}-${var.environment}-private"
-    },
-    var.private_route_table_tags
+        local.common_tags,
+        # roboshop-dev-private
+        {
+            Name = "${var.project}-${var.environment}-private"
+        },
+        var.private_route_table_tags
   )
 }
 
@@ -94,12 +94,12 @@ resource "aws_route_table" "database" {
   vpc_id = aws_vpc.main.id
 
   tags = merge(
-    local.common_tags,
-    # roboshop-dev-database
-    {
-      Name = "${var.project}-${var.environment}-database"
-    },
-    var.database_route_table_tags
+        local.common_tags,
+        # roboshop-dev-database
+        {
+            Name = "${var.project}-${var.environment}-database"
+        },
+        var.database_route_table_tags
   )
 }
 
@@ -110,27 +110,27 @@ resource "aws_route" "public" {
 }
 
 resource "aws_eip" "nat" {
-  domain   = "vpc"
-
+  domain                    = "vpc"
+  
   tags = merge(
-    local.common_tags,
-    {
-      Name = "${var.project}-${var.environment}-nat"
-    },
-    var.eip_tags
+        local.common_tags,
+        {
+            Name = "${var.project}-${var.environment}-nat"
+        },
+        var.eip_tags
   )
 }
 
 resource "aws_nat_gateway" "main" {
   allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public[0].id   # we are creating this in us-east-1 AZ
+  subnet_id     = aws_subnet.public[0].id # we are creating this in us-east-1a AZ
 
   tags = merge(
-    local.common_tags,
-    {
-      Name = "${var.project}-${var.environment}-nat"
-    },
-    var.nat_gateway_tags
+        local.common_tags,
+        {
+            Name = "${var.project}-${var.environment}"
+        },
+        var.nat_gateway_tags
   )
 
   # To ensure proper ordering, it is recommended to add an explicit dependency
@@ -150,23 +150,21 @@ resource "aws_route" "database" {
   nat_gateway_id = aws_nat_gateway.main.id
 }
 
+
 resource "aws_route_table_association" "public" {
   count = length(var.public_subnet_cidrs)
-  
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table_association" "private" {
   count = length(var.private_subnet_cidrs)
-  
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private.id
 }
 
 resource "aws_route_table_association" "database" {
   count = length(var.database_subnet_cidrs)
-  
   subnet_id      = aws_subnet.database[count.index].id
   route_table_id = aws_route_table.database.id
 }
